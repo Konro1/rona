521c7a19e90735a2df7eaf3026875da3cb8b998f
yura myrosh <Konro1@mail.ru>
2013-05-19 23:23:06 +0300

    add loosing data

---
+++ system/config/localconfig.php
@@ -0,0 +1,21 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['installPassword'] = '$6$c193c5b544cb4b88$Ozr4PeEaBN38wWVXIl9ptnR7Icr.0aqatIPyFGsJlxn7h28a3ovVKNhW0h5Ow35u9oq0PnJWuZsL0zFx0y0Yk.';
+$GLOBALS['TL_CONFIG']['encryptionKey'] = '33b09715192206ff558bb3cfaa0ba83b';
+$GLOBALS['TL_CONFIG']['dbDriver'] = 'MySQL';
+$GLOBALS['TL_CONFIG']['dbHost'] = 'localhost';
+$GLOBALS['TL_CONFIG']['dbUser'] = 'root';
+$GLOBALS['TL_CONFIG']['dbPass'] = 'windows';
+$GLOBALS['TL_CONFIG']['dbDatabase'] = 'rona_db';
+$GLOBALS['TL_CONFIG']['dbPconnect'] = false;
+$GLOBALS['TL_CONFIG']['dbCharset'] = 'UTF8';
+$GLOBALS['TL_CONFIG']['dbPort'] = 3306;
+$GLOBALS['TL_CONFIG']['adminEmail'] = 'yriymyrosh@gmail.com';
+$GLOBALS['TL_CONFIG']['timeZone'] = 'Europe/Kiev';
+$GLOBALS['TL_CONFIG']['rewriteURL'] = true;
+$GLOBALS['TL_CONFIG']['defaultUser'] = '';
+$GLOBALS['TL_CONFIG']['defaultGroup'] = '';
+$GLOBALS['TL_CONFIG']['repository_unsafe_catalog'] = false;
+### INSTALL SCRIPT STOP ###
