40deb65c65c2fa27bef081244b99554275ba9ae0
yura myrosh <Konro1@mail.ru>
2013-06-15 20:56:57 +0300

    Pre release

--- system/modules/my_fotogallery/languages/uk/modules.php
+++ system/modules/my_fotogallery/languages/uk/modules.php
@@ -15,29 +15,15 @@
 /**
  * Back end modules
  */
-$GLOBALS['TL_LANG']['MOD']['MSM'] = 'MSM - Технології';
+$GLOBALS['TL_LANG']['MOD']['MY'] = 'Модулі Rona';
 
-$GLOBALS['TL_LANG']['MOD']['msm_university'] = array('Університети','Університети');
-$GLOBALS['TL_LANG']['MOD']['msmPhotoGallery']  = array('Фотогалерея', 'Фотогалерея');
-$GLOBALS['TL_LANG']['MOD']['msmVideoGallery']  = array('Відеогалерея', 'Відеогалерея');
-$GLOBALS['TL_LANG']['MOD']['msmUniversitySearch']  = array('Пошук університетів', 'Пошук університетів');
-$GLOBALS['TL_LANG']['MOD']['msm_Filter']  = array('Фільтр', 'Фільтр');
-$GLOBALS['TL_LANG']['MOD']['msmUniversityList']  = array('Список', 'Список');
-$GLOBALS['TL_LANG']['MOD']['msmUniversityView']  = array('Детальніше про університет', 'Детальніше про університет');
-$GLOBALS['TL_LANG']['MOD']['msmUniversitySpec']  = array('Детальніше про спеціальності', 'Детальніше про спеціальності');
-$GLOBALS['TL_LANG']['MOD']['msmUniversitySlider']  = array('Слайдер', 'Слайдер');
+$GLOBALS['TL_LANG']['MOD']['my_fotogallery'] = array('Фотогалерея','Фотогалерея');
+
 
 
 /**
  * Front end modules
  */
-$GLOBALS['TL_LANG']['FMD']['MSM'] = 'MSM - Технології';
+$GLOBALS['TL_LANG']['FMD']['MY'] = 'Модулі Rona';
 
-$GLOBALS['TL_LANG']['FMD']['msmPhotoGallery']  = array('Фотогалерея', 'Фотогалерея');
-$GLOBALS['TL_LANG']['FMD']['msmVideoGallery']  = array('Відеогалерея', 'Відеогалерея');
-$GLOBALS['TL_LANG']['FMD']['msmUniversitySearch']  = array('Пошук університетів', 'Пошук університетів');
-$GLOBALS['TL_LANG']['FMD']['msm_Filter']  = array('Фільтр', 'Фільтр');
-$GLOBALS['TL_LANG']['FMD']['msmUniversityList']  = array('Список', 'Список');
-$GLOBALS['TL_LANG']['FMD']['msmUniversityView']  = array('Детальніше про університет', 'Детальніше про університет');
-$GLOBALS['TL_LANG']['FMD']['msmUniversitySpec']  = array('Детальніше про спеціальності', 'Детальніше про спеціальності');
-$GLOBALS['TL_LANG']['FMD']['msmUniversitySlider']  = array('Слайдер', 'Слайдер');
+$GLOBALS['TL_LANG']['FMD']['my_fotogallery']  = array('Фотогалерея', 'Фотогалерея');
